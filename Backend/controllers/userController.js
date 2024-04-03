a95dd58f0236564edfe4fd46c6ef46f510d13441
JuanRaV <juanravirgen@gmail.com>
2024-04-02 18:07:20 -0600

    Solving merge conflicts

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -227,7 +227,7 @@ const checkToken = async (req,res)=>{
         const error = new Error("User not found")
         return res.status(404).json({msg:error.message})
     }
-    console.log("Hola")
+    console.log("Hola ADIOS")
 }
 
 export{
