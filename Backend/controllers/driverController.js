3c35797c2c977b2d6621bf6d3f8ccb22beefd9c8
JuanRaV <juanravirgen@gmail.com>
2024-04-03 17:44:50 -0600

    Starting create Raite functionality

--- Backend/controllers/driverController.js
+++ Backend/controllers/driverController.js
@@ -1,7 +1,22 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 
 const createRaite = async (req,res) =>{
-
+    const {startHour, date, start, startingPoint, destination, arrivalPoint, capacity, price, driverId } = req.body
+    const passenger = await prisma.passenger.create({
+        data:{
+            startHour,
+            date,
+            start, 
+            startingPoint,
+            destination,
+            arrivalPoint,
+            capacity,
+            price,
+            driverId
+            // token: 'p' + generateID()
+        }
+    }
+    )
 }
 
 export{
