7d30bf562b35d5b526fee37de085591e27ecd154
JuanRaV <juanravirgen@gmail.com>
2024-04-06 16:56:21 -0600

    Adding extra validations to acceptRaiite function

--- Backend/controllers/passengerController.js
+++ Backend/controllers/passengerController.js
@@ -3,41 +3,49 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 const acceptRaite = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const num = parseInt(id);
-    const passengerId = req.passenger.passengerId
+    try {
+        const { id } = req.params;
+        const num = parseInt(id);
+        const passengerId = req.passenger.passengerId
 
-    const raite = await prisma.raite.findFirst({
-      where: { id :num }
-    });
+        const raite = await prisma.raite.findFirst({
+            where: { id: num }
+        });
 
-    if (!raite) 
-      return res.status(404).json({ error: 'Raite not found' });
-    
-    console.log(raite)
-    // Crea el registro en PassengerRaite
-    const passengerRaite = await prisma.passengerRaite.create({
-        data: {
-          passengerId,
-          raiteId: raite.id
-        }
-      });
-  
-      console.log(passengerRaite);
-  
-      res.json({ message: 'Raite accepted successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+        if (!raite)
+            return res.status(404).json({ error: 'Raite not found' });
+        else if (raite.completed == true)
+            return res.status(404).json({ error: 'Raite already completed' });
+        else if (raite.capacity == 0)
+            return res.status(404).json({ error: 'This raite is full' });
+
+        // Crea el registro en PassengerRaite
+        await prisma.passengerRaite.create({
+            data: {
+                passengerId,
+                raiteId: raite.id
+            }
+        });
+        await prisma.raite.update({
+            where: { id: num },
+            data: {
+                capacity: raite.capacity-1
+            }
+        });
+        
+
+        res.json({ message: 'Raite accepted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 };
 
-const cancelRaite = async(req,res)=>{
+const cancelRaite = async (req, res) => {
 
 }
 
 export {
-  acceptRaite,
-  cancelRaite
+    acceptRaite,
+    cancelRaite
 }
