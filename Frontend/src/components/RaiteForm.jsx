61076eb912cb23660c6956b392570c10b8a0f770
Ramírez <juan_ramirez@baxter.com>
2024-05-12 20:45:43 -0600

    Create Raite form

--- Frontend/src/components/RaiteForm.jsx
+++ Frontend/src/components/RaiteForm.jsx
@@ -6,90 +6,117 @@ import Alert from "./Alert"
 const RaiteForm = () => {
 
     const [idUpdate, setIdUpdate] = useState(null)
-    const [start,setStart] = useState("")
+    const [start, setStart] = useState("")
     const [destination, setDestination] = useState("")
     const [startHour, setStartHour] = useState("")
-    const [date,setDate] = useState("")
+    const [date, setDate] = useState("")
     const [capacity, setCapacity] = useState("")
     const [price, setPrice] = useState("")
     const [startingPoint, setStartingPoint] = useState("")
     const [arrivalPoint, setArrivalPoint] = useState("")
 
-    const {id} = useParams()
+    const { id } = useParams()
 
-    const {showAlert, alert, submitRaite, raite} = useRaites()
+    const { showAlert, alert, submitRaite, raite } = useRaites()
 
-    const handleSubmit = e =>{
+    const handleSubmit = e => {
         e.preventDefault()
+        if([startHour,date,capacity,startingPoint,arrivalPoint].includes('')){
+            showAlert({
+                msg:'All fields are required',
+                error:true
+            })
+            return
+        }
+        else if(capacity>4){
+            showAlert({
+                msg:'The max capacity is 4',
+                error:true
+            })
+            return
+        }
+        else if(capacity<=0){
+            showAlert({
+                msg:'Choose a valid capacity',
+                error:true
+            })
+            return
+        }
+        console.log('All good')
     }
 
-    const {msg} = alert
-  return (
-    <form 
-        className="bg-white py-10 px-5 md:w-1/2 lg:w-3/5 rounded-lg shadow"
-        onSubmit={handleSubmit}
-    >
-        {msg && <Alert alert={alert}/>}
-        <div className="mb-5">
-            <label htmlFor="capacity" className="text-gray-700 uppercase font-bold text-sm">Capacity</label>
-            <input 
-                id="capacity"
-                type="number" 
-                className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
-                placeholder="Ex. 5"
-                value={capacity}
-                onChange={e=>setCapacity(e.target.value)}
-            />
-        </div>
+    const { msg } = alert
+    return (
+        <form
+            className="bg-white py-10 px-5 md:w-1/2 lg:w-3/5 rounded-lg shadow"
+            onSubmit={handleSubmit}
+        >
+            {msg && <Alert alert={alert} />}
+            <div className="mb-5">
+                <label htmlFor="capacity" className="text-gray-700 uppercase font-bold text-sm">Capacity</label>
+                <input
+                    id="capacity"
+                    type="number"
+                    className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
+                    placeholder="Ex. 5"
+                    value={capacity}
+                    onChange={e => setCapacity(e.target.value)}
+                />
+            </div>
 
-        <div className="mb-5">
-            <label htmlFor="date" className="text-gray-700 uppercase font-bold text-sm">Date</label>
-            <input 
-                id="date"
-                type="date" 
-                className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
-                placeholder="Ex. Andares"
-                value={date}
-                onChange={e=>setDate(e.target.value)}
-            />
-        </div>
-        <div className="mb-5">
-            <label htmlFor="hour" className="text-gray-700 uppercase font-bold text-sm">Hour</label>
-            <input 
-                id="hour"
-                type="time" 
-                className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
-                placeholder="Ex. Andares"
-                value={startHour}
-                onChange={e=>setStartHour(e.target.value)}
-            />
-        </div>
-        <div className="mb-5">
-            <label htmlFor="startingPoint" className="text-gray-700 uppercase font-bold text-sm">Starting Point</label>
-            <input 
-                id="startingPoint"
-                type="text" 
-                className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
-                placeholder="Ex. Plaza de Armas"
-                value={startingPoint}
-                onChange={e=>setStartingPoint(e.target.value)}
-            />
-        </div>
+            <div className="mb-5">
+                <label htmlFor="date" className="text-gray-700 uppercase font-bold text-sm">Date</label>
+                <input
+                    id="date"
+                    type="date"
+                    className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
+                    placeholder="Ex. Andares"
+                    value={date}
+                    onChange={e => setDate(e.target.value)}
+                />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="hour" className="text-gray-700 uppercase font-bold text-sm">Hour</label>
+                <input
+                    id="hour"
+                    type="time"
+                    className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
+                    placeholder="Ex. Andares"
+                    value={startHour}
+                    onChange={e => setStartHour(e.target.value)}
+                />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="startingPoint" className="text-gray-700 uppercase font-bold text-sm">Starting Point</label>
+                <input
+                    id="startingPoint"
+                    type="text"
+                    className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
+                    placeholder="Ex. Plaza de Armas"
+                    value={startingPoint}
+                    onChange={e => setStartingPoint(e.target.value)}
+                />
+            </div>
 
-        <div className="mb-5">
-            <label htmlFor="arrivalPoint" className="text-gray-700 uppercase font-bold text-sm">Starting Point</label>
+            <div className="mb-5">
+                <label htmlFor="arrivalPoint" className="text-gray-700 uppercase font-bold text-sm">Starting Point</label>
+                <input
+                    id="arrivalPoint"
+                    type="text"
+                    className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
+                    placeholder="Ex. Andares"
+                    value={arrivalPoint}
+                    onChange={e => setArrivalPoint(e.target.value)}
+                />
+            </div>
             <input 
-                id="arrivalPoint"
-                type="text" 
-                className="border w-full p-2 mt-2 placeholder:gray-400 rounded-md"
-                placeholder="Ex. Andares"
-                value={arrivalPoint}
-                onChange={e=>setArrivalPoint(e.target.value)}
+                type="submit"
+                value={idUpdate?'Update Raite' : 'Create Raite'}
+                className="bg-indigo-600 w-full p-3 uppercase font-bold text-white rounded cursor-pointer hover:bg-indigo-800 transition-colors"
+                
             />
-        </div>
-
-    </form>
-  )
+        </form>
+    )
 }
 
 export default RaiteForm
