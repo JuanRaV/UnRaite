639e58ba0b2b10d9c2a6840ca1c4e3bf97b06253
Ramírez <juan_ramirez@baxter.com>
2024-05-21 16:27:24 -0600

    Adding origin and destination message

--- Frontend/src/pages/PassengerMain.jsx
+++ Frontend/src/pages/PassengerMain.jsx
@@ -7,13 +7,13 @@ import RaitePreviewPassenger from "../components/RaitePreviewPassenger"
 
 const PassengerMain = () => {
     let { raites } = useRaites()
-    const {auth} = useAuth()
+    const { auth } = useAuth()
     const [towns, setTowns] = useState([])
     const [raitesFilter, setRaitesFilter] = useState([])
     const [option, setOption] = useState("")
     const [start, setStart] = useState("")
     const [destination, setDestination] = useState("")
-    const [currentRaite,setCurrentRaite] = useState({})
+    const [currentRaite, setCurrentRaite] = useState({})
     raites = raites.allRaites
     console.log(auth.raite[0]?.raiteId)
     console.log(raites)
@@ -30,27 +30,27 @@ const PassengerMain = () => {
         fetchTowns()
     }, [])
 
-    useEffect(()=>{
-        const raitesFiltrados =raites?.filter(raite=>{
-           return raite.start == start && raite.destination==destination
+    useEffect(() => {
+        const raitesFiltrados = raites?.filter(raite => {
+            return raite.start == start && raite.destination == destination
         })
         setRaitesFilter(raitesFiltrados)
-    },[start,destination])
+    }, [start, destination])
 
-    useEffect(()=>{
-        const getCurrentRaite = ()=>{
+    useEffect(() => {
+        const getCurrentRaite = () => {
             const currentId = auth.raite[0]?.raiteId
-            const currentRaite = raites?.filter(raite=>{
-                return raite.id==currentId
+            const currentRaite = raites?.filter(raite => {
+                return raite.id == currentId
             })
             setCurrentRaite(currentRaite)
         }
         getCurrentRaite()
-    },[])
+    }, [])
     // console.log(currentRaite[0]?)
 
 
-    const seeAll=()=>{
+    const seeAll = () => {
         setStart("")
         setDestination("")
     }
@@ -61,11 +61,11 @@ const PassengerMain = () => {
     const { msg } = alert
     return (
         <>
-            <h1 className="text-4xl font-black">All Raites Available</h1>
+            <h1 className="text-3xl font-bold mt-0 mb-2">Are you  going or leaving to GDL?</h1>
             {msg && <Alert alert={alert} />}
-            <div className=" text-center">
-                <div className=" text-center">
-                    <h2 className="font-bold text-xl uppercase text-center mb-5">Are you  going or leaving to GDL?</h2>
+            <div className=" flex text-center">
+                <div className="  text-center">
+                    {/* <h2 className="font-bold text-xl uppercase text-center mb-2 mt-5">Are you  going or leaving to GDL?</h2> */}
                     <label className="mx-5 text-indigo-500 font-bold text-xl">
                         <input
                             type="radio"
@@ -98,67 +98,76 @@ const PassengerMain = () => {
                         />
                         Leaving
                     </label>
-                </div>
 
-                <div className="flex justify-center mt-5">
-                    {option == "voy" ? (
-                        <>
+                    <div className="flex-col justify-center mt-3">
+                        {option == "voy" ? (
+                            <>
 
-                            <div>
-                                <p className="text-gray-700 uppercase font-bold text-sm mt-5 text-center">Select Your Origin</p>
-                                {/* {setStart(e.target.value.townName);setPrice(e.target.value.price)} */}
-                                <select onChange={(e) => {
-                                    setDestination("Guadalajara")
-                                    setStart(e.target.value);
-                                    // console.log(start)
-                                }}
-                                    className="mb-5 border p-1 rounded-lg">
-                                    {towns.map(town => (
-
-                                        <option key={town.id} value={town.name}>
-                                            {town.townName}
-                                        </option>
-
-                                    ))}
-                                </select>
-                            </div>
-                        </>
-
-                    ) : (
-                        <>
-
-                            <div>
-                                <p className="text-gray-700 uppercase font-bold text-sm mt-5 text-center">Select your Destination</p>
-                                <select
-                                    onChange={(e) => {
-
-                                        // console.log(e.target.value)
-                                        setDestination(e.target.value);
-                                        setStart("Guadalajara")
+                                <div>
+                                    <p className="text-gray-700 uppercase font-bold text-sm mt-5 text-center">Select Your Origin</p>
+                                    {/* {setStart(e.target.value.townName);setPrice(e.target.value.price)} */}
+                                    <select onChange={(e) => {
+                                        setDestination("Guadalajara")
+                                        setStart(e.target.value);
+                                        // console.log(start)
                                     }}
-                                    className="mb-5 border p-1 rounded-lg">
-                                    {towns.map(town => (
-                                        <option key={town.id} value={town.townName}>
-                                            {town.townName}
-                                        </option>
-                                    ))}
-                                </select>
+                                        className="mb-5 border p-1 rounded-lg">
+                                        {towns.map(town => (
 
-                            </div>
-                        </>
-                    )}
+                                            <option key={town.id} value={town.name}>
+                                                {town.townName}
+                                            </option>
+
+                                        ))}
+                                    </select>
+                                </div>
+                            </>
+
+                        ) : (
+                            <>
+
+                                <div>
+                                    <p className="text-gray-700 uppercase font-bold text-sm mt-5 text-center">Select your Destination</p>
+                                    <select
+                                        onChange={(e) => {
+
+                                            // console.log(e.target.value)
+                                            setDestination(e.target.value);
+                                            setStart("Guadalajara")
+                                        }}
+                                        className="mb-5 border p-1 rounded-lg">
+                                        {towns.map(town => (
+                                            <option key={town.id} value={town.townName}>
+                                                {town.townName}
+                                            </option>
+                                        ))}
+                                    </select>
+
+                                </div>
+                            </>
+                        )}
+                    </div>
                 </div>
+                {start || destination ? (
+                    <div className="flex flex-col w-1/2 justify-center">
+                        <h2 className="text-gray-700 uppercase font-bold text-xl mt-5">From: <span className="text-indigo-500">{start}</span></h2>
+                        <h2 className="text-gray-700 uppercase font-bold text-xl mt-5">To: <span className="text-indigo-500">{destination}</span></h2>
+                    </div>
+                ) : (
+                    <input type="hidden"/>
+                )}
+
 
             </div>
             <h2 className="text-gray-700 uppercase font-bold text-xl mt-5">Current Raite</h2>
             <div className="bg-white shadow mt-3 rounded-lg">
-                {currentRaite?.length?(
+                {currentRaite?.length ? (
                     <RaitePreviewPassenger
                         key={currentRaite[0]?.id}
                         raite={currentRaite[0]}
-                        
+
                     />
-                ):(
+                ) : (
                     <p className="p-5 text-center text-gray-600 uppercase font-bold">No <span className="text-indigo-300">Raites</span> Reserved</p>
                 )}
             </div>
@@ -173,18 +182,18 @@ const PassengerMain = () => {
                             destination={destination}
                         />
                     ))
-                    :raitesFilter?.length == 0 && start!=""?(
+                    : raitesFilter?.length == 0 && start != "" ? (
                         <p className="p-5 text-center text-gray-600 uppercase font-bold">No <span className="text-indigo-300">Raites</span> Available</p>
                     )
-                    : raites?.length ?
-                    raites.map(raite => (
-                        <RaitePreviewPassenger
-                            key={raite.id}
-                            raite={raite}
-                            origin={origin}
-                            destination={destination}
-                        />
-                    )): <p className="p-5 text-center text-gray-600 uppercase font-bold">No <span className="text-indigo-300">Raites</span> Available</p>
+                        : raites?.length ?
+                            raites.map(raite => (
+                                <RaitePreviewPassenger
+                                    key={raite.id}
+                                    raite={raite}
+                                    origin={origin}
+                                    destination={destination}
+                                />
+                            )) : <p className="p-5 text-center text-gray-600 uppercase font-bold">No <span className="text-indigo-300">Raites</span> Available</p>
                 }
             </div>
         </>
