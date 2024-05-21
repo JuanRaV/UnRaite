a89e2d4dcb3a140db62a2702cbaf052f31e94a19
Ramírez <juan_ramirez@baxter.com>
2024-05-20 23:11:18 -0600

    Filtering raites

--- Backend/controllers/passengerController.js
+++ Backend/controllers/passengerController.js
@@ -201,10 +201,37 @@ const strike = async (req, res) => {
     }
 }
 
+const current = async (req, res) => {
+    const {id} =req.params
+    const num = parseInt(id)
+    try {
+        const { passengerId } = req.passenger;
+        // console.log("req pass", req.passenger)
+        const inProgressRides = await prisma.passengerRaite.findMany({
+            where: {
+                passengerId: passengerId,
+                raiteId:num,
+                raite: {
+                    completed: false
+                }
+            },
+            include: {
+                raite: true
+            }
+        });
+        console.log(inProgressRides)
+        if(!inProgressRides)
+        res.status(200).json(inProgressRides);
+    } catch (error) {
+        console.error('Error checking passenger rides in progress:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
 export {
     getAllRaites,
     getOneRaite,
     acceptRaite,
     cancelRaite,
-    strike
+    strike,
+    current
 }
--- Backend/routes/passengerRoutes.js
+++ Backend/routes/passengerRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllRaites,getOneRaite,acceptRaite, cancelRaite, strike } from '../controllers/passengerController.js';
+import { getAllRaites,getOneRaite,acceptRaite, cancelRaite, strike, current } from '../controllers/passengerController.js';
 import { checkPassengersAuth } from '../middleware/checkAuth.js';
 
 const router = express.Router()
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/',checkPassengersAuth, getAllRaites)
 router.get('/:id',checkPassengersAuth, getOneRaite)
 router.put("/accept-raite/:id", checkPassengersAuth, acceptRaite)
+router.get("/current-raite/:id",checkPassengersAuth, current)
 router.delete("/cancel-raite/:id", checkPassengersAuth, cancelRaite)
 router.post("/strike-driver/:driverId/:raiteId", checkPassengersAuth, strike )
 
